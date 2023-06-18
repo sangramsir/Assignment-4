4fef168a377edeccb23c36c899f6e841261a545f
sangramsir <136458988+sangramsir@users.noreply.github.com>
2023-06-18 18:26:20 +0530

    Add files via upload

---
+++ Ass-4.py
@@ -0,0 +1,43 @@
+# Use of for loop in python 
+sum=0
+for i in range (15):
+    sum=sum+i
+    print(sum)
+#While loop 
+
+i=1
+
+while i<=10:
+    if i%2==1:
+        print(i)
+    else:
+        print()
+    i=i+1
+
+for i in range(1,20):
+    if i==12:
+        break
+    print(i)
+else:
+      print("Loop ends normally")
+for r in range(1,13):
+     print(" Value of i is",i)
+     if(i==7):
+          continue
+     print("Hello Man")
+     print("Hello Sangram")
+     print()
+
+     i=1
+     while i<10:
+          if i==5:
+               pass
+          print(i)
+          i=i+1
+          print("Excecuted sucessfully")
+
+    
+
+
+    
+
